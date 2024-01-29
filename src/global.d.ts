991b5285234cecb4bbb23ed7966c7e505f7bdd1d
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-01-29 15:09:24 +0100

    Init

---
+++ src/global.d.ts
@@ -0,0 +1,71 @@
+// images
+declare module '*.apng' {
+    const src: string
+    export default src
+}
+declare module '*.png' {
+    const src: string
+    export default src
+}
+declare module '*.jpg' {
+    const src: string
+    export default src
+}
+declare module '*.jpeg' {
+    const src: string
+    export default src
+}
+declare module '*.jfif' {
+    const src: string
+    export default src
+}
+declare module '*.pjpeg' {
+    const src: string
+    export default src
+}
+declare module '*.pjp' {
+    const src: string
+    export default src
+}
+declare module '*.gif' {
+    const src: string
+    export default src
+}
+declare module '*.svg' {
+    const src: string
+    export default src
+}
+declare module '*.ico' {
+    const src: string
+    export default src
+}
+declare module '*.webp' {
+    const src: string
+    export default src
+}
+declare module '*.avif' {
+    const src: string
+    export default src
+}
+
+// Shaders
+declare module '*.frag' {
+    const src: string
+    export default src
+}
+declare module '*.vert' {
+    const src: string
+    export default src
+}
+declare module '*.glsl' {
+    const src: string
+    export default src
+}
+declare module '*.vs' {
+    const src: string
+    export default src
+}
+declare module '*.fs' {
+    const src: string
+    export default src
+}
